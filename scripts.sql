e4c1aae611319003c29bd742bd721d4c919b298f
Bruno Bitencourt Luiz <brunowdev@gmail.com>
2017-04-29 18:38:38 -0300

    Query que exibe os usuários do banco

--- scripts.sql
+++ scripts.sql
@@ -3,3 +3,5 @@
 select * from v$parameter - Exibe todos os parâmetros do banco
 
 select * from v$session - Exibe todos os processos rodando em segundo plano
+
+select * from dba_users - Exibe todos os usuários do banco

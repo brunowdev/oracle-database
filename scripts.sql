de29a5c944b1c664fa882aba4925afecc4a648ad
Bruno Bitencourt Luiz <brunowdev@gmail.com>
2017-04-29 18:39:29 -0300

    Melhorada query que lista usuários

--- scripts.sql
+++ scripts.sql
@@ -4,4 +4,4 @@ select * from v$parameter - Exibe todos os parâmetros do banco
 
 select * from v$session - Exibe todos os processos rodando em segundo plano
 
-select * from dba_users - Exibe todos os usuários do banco
+select * from dba_users/select username from dba_users - Exibe todos os usuários do banco

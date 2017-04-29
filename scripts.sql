f5b15ba8ca5459175d5ad04385bf4af4992e6ca7
Bruno Bitencourt Luiz <brunowdev@gmail.com>
2017-04-29 18:41:42 -0300

    comando de listagem de usuários

--- scripts.sql
+++ scripts.sql
@@ -5,3 +5,5 @@ select * from v$parameter - Exibe todos os parâmetros do banco
 select * from v$session - Exibe todos os processos rodando em segundo plano
 
 select * from dba_users/select username from dba_users - Exibe todos os usuários do banco
+
+select * from all_users - Lista todos os usuários/schemas, ids e data de criação
